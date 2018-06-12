fa2db89a74fae0d7cff62c3ba3128660cb1d8cc2
Owen <owenyuwono@gmail.com>
2018-06-12 18:11:01 +0700

    modify database.php for heroku deploy

--- application/config/database.php
+++ application/config/database.php
@@ -73,24 +73,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> 'pgsql:host=localhost;dbname=cliquedb',
-	'hostname' => 'localhost',
-	'username' => 'postgres',
-	'password' => 'root',
-	'database' => 'cliquedb',
-	'dbdriver' => 'pdo',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+$url = "pgsql://postgres:root@localhost:5432/cliquedb";
+// Else, if running on Heroku
+if(isset($_ENV['CLEARDB_DATABASE_URL'])){
+	$url = $_ENV['CLEARDB_DATABASE_URL'];
+}
+
+// Parse connection url
+$connection_info = parse_url($url);
+
+define('DB_HOST', $connection_info['host']);
+define('DB_DATABASE', substr($connection_info['path'], 1));
+define('DB_USER', $connection_info['user']);
+define('DB_PASS', $connection_info['pass']);
+
+$db['default']['hostname'] = constant("DB_HOST");
+$db['default']['username'] = constant("DB_USER");
+$db['default']['password'] = constant("DB_PASS");
+$db['default']['database'] = constant("DB_DATABASE");
+$db['default']['dbdriver'] = 'pdo';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
