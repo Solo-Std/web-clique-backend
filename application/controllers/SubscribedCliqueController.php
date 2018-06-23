9fd7a230ef7d9824b2738c1d9a85a97397f821f2
Owen <owenyuwono@gmail.com>
2018-06-23 20:34:07 +0700

    add subscribed clique controller

---
+++ application/controllers/SubscribedCliqueController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 23/06/2018
+ * Time: 20.29
+ */
+
+class SubscribedCliqueController extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->database();
+        $this->load->model('SubscribedCliqueModel');
+    }
+}
