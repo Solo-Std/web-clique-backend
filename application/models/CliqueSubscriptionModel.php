ba598c6e53545c2e15088afc35400fe2c17593a7
Adjie Wahyu Wicaksono <Adjie.wahyu30@gmail.com>
2018-06-27 19:15:00 +0700

    whaaaat

--- application/models/CliqueSubscriptionModel.php
+++ application/models/CliqueSubscriptionModel.php
@@ -16,7 +16,7 @@ class CliqueSubscriptionModel extends CI_Model
     public function addSubscription($data)
     {
         $this->db->select('clique_id');
-        $this->db->where('title', $data['clique_name']);
+        $this->db->where('clique_name', $data['clique_name']);
         $clique_id = $this->db->get('clique_master');
 
         $subscription = array(
@@ -25,7 +25,5 @@ class CliqueSubscriptionModel extends CI_Model
         );
 
         $this->db->insert('subscribed_clique_relation', $subscription);
-
-        return true;
     }
 }
