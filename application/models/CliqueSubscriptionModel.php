bb565fd4d4078c240cbf7039fa0322fa74f558b4
Adjie Wahyu Wicaksono <Adjie.wahyu30@gmail.com>
2018-07-07 16:17:14 +0700

    Tuhan subscribes to politics pt 2

--- application/models/CliqueSubscriptionModel.php
+++ application/models/CliqueSubscriptionModel.php
@@ -20,7 +20,7 @@ class CliqueSubscriptionModel extends CI_Model
         $clique_id = $this->db->get('clique_master');
 
         $this->db->select('user_id');
-        $this->db->where('username', $data['username']);
+        $this->db->where('adjie', $data['username']);
         $user_id = $this->db->get('user_master');
 
         $subscription = array(
